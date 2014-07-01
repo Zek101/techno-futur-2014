3e2c620b0a8c24bc3c62a93b916f92d35299eb16
ahmed.zarioh <ahmed.zarioh@gmail.com>
2014-07-01 09:32:32 +0000

    gestion des commentaires

---  techno-futur-2014/Lotto4/src/be/wavenet/lotto/Lotto.java
+++  techno-futur-2014/Lotto4/src/be/wavenet/lotto/Lotto.java
@@ -9,12 +9,10 @@ public class Lotto implements Serializable {
 	private static final int MAX = 42;
 	private static final int SIZE = 6;
 	private ArrayList<Integer> result = new ArrayList<Integer>();
-
+	// salut ahmedZarioh
 	public ArrayList<Integer> getResult() {
 		// Bom dia a todos! Bruno 
 		// Bom dia a todos! Bruno 
-
-		
 		// Just return result to calling method
 		return this.result;
 	}
